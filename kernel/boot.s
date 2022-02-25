9cc339a63f30c234bbb503278ad6672139690343
aiyo <1608450902@qq.com>
2022-02-25 16:58:28 +0800

    nothing

--- kernel/boot.s
+++ kernel/boot.s
@@ -1,9 +1,9 @@
 MAGIC equ 0x1badb002
 FLAGS equ 0x00
 
-	section .text
+section .text
 
-grubBoot:
+;grubBoot:
 	dd MAGIC
 	dd FLAGS
 	dd -(MAGIC + FLAGS)
@@ -14,12 +14,154 @@ grubBoot:
 ; After that, I find the writing way of grubBoot!
 ; db 0x02,0xb0,0xad,0x1b
 ; db 0x00,0x00,0x00,0x00
-; db 0xfe,0x4f,0x52,0xe4
+; db 0xfe,0x4f,0x62,0xe4
 ; -----------------------------------------------
 
-	global _start
-	extern kmain
-_start:
-	; cli
-	call kmain
+extern clear
+extern print
+extern println
+extern print_logo
+extern print8bytes
+extern print_int
+
+startup:
+	cld
+	; Here, I can't change value of esp! Why???
+	mov esp, 0
+	mov ebp, esp
+
+	call clear
+	call print_logo
+
+	push 8
+	push L_MAG
+	call print
+	add esp, 0x8
+	xor eax, eax
+	mov eax, 0x1badb002
+	push eax
+	call print8bytes
+	add esp, 0x4
+
+	push 6
+	push L_ESP
+	call print
+	add esp, 0x8
+	xor eax, eax
+	mov eax, esp
+	push eax
+	call print8bytes
+	add esp, 0x4
+
+	push 6
+	push L_EBP
+	call print
+	add esp, 0x8
+	xor eax, eax
+	mov eax, ebp
+	push eax
+	call print8bytes
+	add esp, 0x4
+
+	push 6
+	push L_CS
+	call print
+	add esp, 0x8
+	xor eax, eax
+	mov ax, cs
+	push eax
+	call print8bytes
+	add esp, 0x4
+
+	push 6
+	push L_SS
+	call print
+	add esp, 0x8
+	xor eax, eax
+	mov ax, cs
+	push eax
+	call print8bytes
+	add esp, 0x4
+
+	push 6
+	push L_DS
+	call print
+	add esp, 0x8
+	xor eax, eax
+	mov ax, cs
+	push eax
+	call print8bytes
+	add esp, 0x4
+
+	push 6
+	push L_ES
+	call print
+	add esp, 0x8
+	xor eax, eax
+	mov ax, es
+	push eax
+	call print8bytes
+	add esp, 0x4
+
+	push 6
+	push L_GS
+	call print
+	add esp, 0x8
+	xor eax, eax
+	mov ax, gs
+	push eax
+	call print8bytes
+	add esp, 0x4
+
+	push 6
+	push L_CR0
+	call print
+	add esp, 0x8
+	xor eax, eax
+	mov eax, cr0
+	push eax
+	call print8bytes
+	add esp, 0x4
+
+	push 6
+	push L_A20
+	call print
+	add esp, 0x8
+	xor eax, eax
+	in al, 0x92
+	push eax
+	call print8bytes
+	add esp, 0x4
+
+	mov eax, [ask_addr_begin]
+	push eax
+	mov eax, [ask_addr_begin + 1]
+	push eax
+	mov eax, [ask_addr_begin + 2]
+	push eax
+	mov eax, [ask_addr_begin + 3]
+	push eax
+	push 4
+	mov eax, esp
+	add eax, 4
+	push eax
+	call print_int
+	add esp, 24
+
 	hlt
+
+section .data
+L_CS:  db " cs:0x"
+L_SS:  db " ss:0x"
+L_DS:  db " ds:0x"
+L_ES:  db " es:0x"
+L_GS:  db " gs:0x"
+L_ESP: db "esp:0x"
+L_EBP: db "ebp:0x"
+L_CR0: db "cr0:0x"
+L_A20: db "A20:0x"
+L_MAG: db "MAGIC:0x"
+
+ask_addr_begin equ 0x8000
+
+
--- kernel/kmain.c
+++ kernel/kmain.c
@@ -1,19 +1,80 @@
-void kmain(void) {
-	char* str = "Hello World！";
-	char* vidptr = (char*)0xb8000;
-	unsigned int i = 0;
-	unsigned int j = 0;
-	while(j < 80 * 25 * 2) {
-		vidptr[j] = ' ';
-		vidptr[j+1] = 0x07;
-		j = j + 2;
+#define MEMORY_VGA 0xb8000
+#define VGA_WIDTH 80
+#define VGA_HEIGHT 25
+
+#define TOCH(e) e < 10 ? 48 + e : 87 + e
+
+static int vga_c = 0, vga_l = 0;
+
+void clear();
+void print_logo();
+void print8bytes(int value);
+void print(char *ptr, unsigned int length);
+void println(char *ptr, unsigned int length);
+void print_int(int *p, unsigned int length);
+
+void print_logo() {
+	println("DJun.OS", 7);
+}
+
+void print8bytes(int value) {
+	char buf[25] = {0};
+	buf[7] = TOCH((unsigned char) (value & 0x0f));
+	buf[6] = TOCH((unsigned char) ((value >> 4) & 0x0f));
+	buf[5] = TOCH((unsigned char) ((value >> 8) & 0x0f));
+	buf[4] = TOCH((unsigned char) ((value >> 12) & 0x0f));
+	buf[3] = TOCH((unsigned char) ((value >> 16) & 0x0f));
+	buf[2] = TOCH((unsigned char) ((value >> 20) & 0x0f));
+	buf[1] = TOCH((unsigned char) ((value >> 24) & 0x0f));
+	buf[0] = TOCH((unsigned char) ((value >> 28) & 0x0f));
+	println(buf, 8);
+}
+
+void print_int(int *p, unsigned int length) {
+	for (unsigned int i = 0; i < length; i++) {
+		print("0x", 2);
+		print8bytes(p[i]);
 	}
-	j = 0;
-	while(str[j] != '\0') {
-        vidptr[i] = str[j];
-        vidptr[i+1] = 0x07;
-        ++j;
-        i = i + 2;
-    }
-    return;
+}
+
+void clear() {
+	char *p_vga = (char *) MEMORY_VGA;
+	for (unsigned int j = 0; j < VGA_HEIGHT; j++) {
+		for (unsigned int i = 0; i < VGA_WIDTH; i++) {
+			p_vga[2 * (j * VGA_WIDTH + i)] = 0;
+			p_vga[2 * (j * VGA_WIDTH + i) + 1] = 0x07;
+		}
+	}
+}
+
+void print(char *ptr, unsigned int length)
+{
+	char *p_vga = (char *) MEMORY_VGA;
+	unsigned int j = vga_c, k = vga_l;
+	for (unsigned int i = 0; i < length; i++) {
+		if (j >= VGA_WIDTH) {
+			j = 0;
+			k = (k + 1 >= VGA_HEIGHT) ? 0 : k + 1;
+		}
+		switch (ptr[i]) {
+			case '\r':
+				j = 0;
+				continue;
+			case '\n':
+				j = 0;
+				k = (k + 1 >= VGA_HEIGHT) ? 0 : k + 1;
+				continue;
+			default:;
+		}
+		p_vga[2 * (k * VGA_WIDTH + j)] = ptr[i];
+		p_vga[2 * (k * VGA_WIDTH + j) + 1] = 0x07;
+		j++;
+	}
+	vga_c = j >= VGA_WIDTH ? 0 : j;
+	vga_l = j >= VGA_WIDTH ? (k + 1) : k;
+}
+
+void println(char *ptr, unsigned int length) {
+	print(ptr, length);
+	print("\n", 1);
 }
